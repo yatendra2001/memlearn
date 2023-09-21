f8c23c597fd9741382c149cf658546aa909d3db2
Yatendra Kumar <yatendraroria2001@gmail.com>
2023-09-21 21:28:04 +0530

    chores: initialized project

---
+++ android/app/src/main/kotlin/com/superawesomeapps/memlearn/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.superawesomeapps.memlearn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:memlearn/src/home/view/home_page.dart';
+
+class MemLearn extends StatelessWidget {
+  const MemLearn({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      restorationScopeId: 'app',
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''), // English, no country code
+      ],
+      onGenerateTitle: (BuildContext context) =>
+          AppLocalizations.of(context)!.appTitle,
+      theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+      onGenerateRoute: (RouteSettings routeSettings) {
+        return MaterialPageRoute<void>(
+          settings: routeSettings,
+          builder: (BuildContext context) {
+            switch (routeSettings.name) {
+              default:
+                return const HomePage();
+            }
+          },
+        );
+      },
+    );
+  }
+}
